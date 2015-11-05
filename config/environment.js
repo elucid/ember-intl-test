2b631845baf94b68019c308a5cf939223edd977c
Justin Giancola <justin.giancola@gmail.com>
2015-11-04 17:42:14 -0800

    Add ember-intl (breaks production sourcemaps)

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,10 @@ module.exports = function(environment) {
       }
     },
 
+    intl: {
+      defaultLocale: 'en-us' // default value
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
